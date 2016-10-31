3d9d104c53bb82318e717b1ac473265be9906ff8
Roshan <rts@webshipr.com>
2016-10-31 10:55:02 +0100

    reuse components in other components

--- app/assets/javascripts/components/records.js.jsx
+++ app/assets/javascripts/components/records.js.jsx
@@ -13,12 +13,39 @@ var Records = React.createClass({
     this.setState({ records: records });
   },
 
+  credits: function() {
+    var credits = this.state.records.filter(function(val){
+      return val.amount >= 0
+    });
+    return credits.reduce(function(prev, curr){
+      return prev + parseFloat(curr.amount);
+    }, 0)
+  },
+
+  debits: function() {
+    var debits = this.state.records.filter(function(val){
+      return val.amount < 0
+    });
+    return debits.reduce(function(prev, curr){
+      return prev + parseFloat(curr.amount);
+    }, 0)
+  },
+
+  balance: function() {
+    return this.debits() + this.credits();
+  },
+
   render: function() {
     return(
       <div className='records'>
         <h2 className='title'>
           Records
         </h2>
+        <div className='row'>
+          <AmountBox type='success' amount={this.credits()} text='Credit' />
+          <AmountBox type='danger' amount={this.debits()} text='Debit' />
+          <AmountBox type='info' amount={this.balance()} text='Balance' />
+        </div>
         <RecordForm handleNewRecord={this.addRecord} />
         <hr />
         <table className='table table-bordered'>
